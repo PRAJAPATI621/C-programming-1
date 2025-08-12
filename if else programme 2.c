4301dc5cac74c313af97fc2a2c0a7ca8172d35f7
PRAJAPATI621 <mahiprajapati20308@gmail.com>
2025-08-12 17:39:28 +0530

    Add files via upload

---
+++ if else programme 2.c
@@ -0,0 +1,31 @@
+// Online C complire to run C program online
+#include<stdio.h>
+int main()
+{
+ int a,b;
+ printf("Enter two number\n");
+ scanf("%d %d",&a,&b);
+  if(a>b)
+ {
+ printf("%d is the larger number and %d is the smaller number among the number",a,b);
+ }
+ else
+ {
+  if(a<b)
+  {
+   printf("%d is the smaller number and %d is the larger number among the number",a,b);
+   }
+ else
+ {
+  printf("both number are equal");
+ }
+ }
+ return 0;
+}
+
+
+
+
+
+
+
