0740f96b5dfdec3954d8c28fe223e6aa717cf3e4
PRAJAPATI621 <mahiprajapati20308@gmail.com>
2025-08-12 17:03:11 +0530

    Add files via upload

---
+++ if else programme.c
@@ -0,0 +1,26 @@
+// Online C complire to run C program online
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ printf("Enter number\n");
+ int number;
+ scanf("%d",&number);
+  if(number%2==0)
+ {
+ printf("Number is Even");
+ }
+ else
+ {
+ printf("Number is Odd");
+ }
+ return 0;
+ clrscr();
+}
+
+
+
+
+
+
+
