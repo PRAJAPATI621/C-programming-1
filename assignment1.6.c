6d7f16c3d6f446735baa9d65302d727c41713f7c
PRAJAPATI621 <mahiprajapati20308@gmail.com>
2025-08-14 00:00:31 +0530

    Add files via upload

---
+++ assignment1.6.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{/*Program to convert hour into minute 1 Nos.*/
+ int a,b;
+ printf("Enter 1 Nos");
+ scanf("%d",&a);
+ b = a*60;
+ printf("%d * 60 = %d\n",a,b,c);
+}
