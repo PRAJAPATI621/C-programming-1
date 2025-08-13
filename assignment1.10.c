30d86ff435a246bd86539b30195d623a9cf6db54
PRAJAPATI621 <mahiprajapati20308@gmail.com>
2025-08-14 00:43:55 +0530

    Add files via upload

---
+++ assignment1.10.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{/*Program to dollars into pound where 1$ = 1.46 pound.*/
+ int a,b;
+ printf("Enter 1 Nos.");
+ scanf("%d",&a);
+ b = a*1.46;
+ printf("%d * 1.46 = %d\n,a,b);
+}
