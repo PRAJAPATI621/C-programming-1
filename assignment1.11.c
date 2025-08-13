8634a000293227272a8301ac476f67429e52a8ec
PRAJAPATI621 <mahiprajapati20308@gmail.com>
2025-08-14 00:52:21 +0530

    Add files via upload

---
+++ assignment1.11.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{/*Program to grams into kg where 1gram = 0.001.*/
+ int a,b;
+ printf("Enter 1 Nos.");
+ scanf("%d",&a);
+ b = a*0.001;
+ printf("%d * 0.001 = %d\n,a,b);
+}
