0d1ed2b5a9b21bd348b8b3097f21607b9e36c050
PRAJAPATI621 <mahiprajapati20308@gmail.com>
2025-08-14 00:32:23 +0530

    Add files via upload

---
+++ assignment1.9.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{/*Program to convert Rs. into dollars where 1$ = 48 Rs.*/
+ int a,b;
+ printf("Enter 1 Nos.");
+ scanf("%d",&a);
+ b a/48;
+ printf("%d / 48",a,b);
+} 
